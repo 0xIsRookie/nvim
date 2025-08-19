00bd01b577ce58fe4e26765cc94ddc746fc54d42
0xIsRookie <IsR00kie@outlook.com>
2025-08-20 00:51:10 +0800

    添加 多窗口分割线颜色, 开启拼写检查

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
 require("config.lazy")
+
+vim.cmd([[highlight WinSeparator guifg=#FF8700 guibg=NONE ctermfg=208 ctermbg=NONE]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,7 @@
 
 -- 自动换行
 vim.opt.wrap = true
+-- 开启拼写检查
+vim.opt.spell = true
+-- 设置拼写语言为英文和简体中文
+vim.opt.spelllang = { "en_us", "cjk" }
